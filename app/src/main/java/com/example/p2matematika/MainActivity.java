a79cd6acb227eef4a1abbd4d449de22c3e0dc22e
Paaske11 <djjepspage@live.dk>
2020-04-03 14:25:01 +0200

    Test 3

--- app/src/main/java/com/example/p2matematika/MainActivity.java
+++ app/src/main/java/com/example/p2matematika/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       //I am the king of us
+       //HEIII TIL DIIIG
 
 
     }
