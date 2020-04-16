54166f2550ea1047e3ac6f818c60aea7cea72e95
Lasse11551 <56248206+Lasse11551@users.noreply.github.com>
2020-04-16 15:08:12 +0200

    New pages - 3 x hjælp sider til plus - visuel, læsende og kinestætisk

---
+++ app/src/main/java/com/example/p2matematika/Plushjaelpkinestaetisk.java
@@ -0,0 +1,14 @@
+package com.example.p2matematika;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Plushjaelpkinestaetisk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plushjaelpkinestaetisk);
+    }
+}
--- app/src/main/java/com/example/p2matematika/Plushjaelplaesende.java
+++ app/src/main/java/com/example/p2matematika/Plushjaelplaesende.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class introbrugertest extends AppCompatActivity {
+public class Plushjaelplaesende extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_introbrugertest);
+        setContentView(R.layout.activity_plushjaelplaesende);
     }
 }
